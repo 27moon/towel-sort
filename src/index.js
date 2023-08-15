18639df44b459a04be131ac78e2d67693ab87853
Marta <emeraldraven27@gmail.com>
2023-08-15 22:11:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 
 // You should implement your task here.
+function towel (matrix) {
+
+}
+
+  
+  
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  if (matrix === undefined || matrix.length === 0) {
+   return [];
+  } else {
+  let result =  matrix.filter((item, index) => (index % 2)); //отделяем только четные элементы 
+  for (let value of result) {
+    value.reverse();
+  }
+}
+  let final = matrix.join(); //получили строку
+
+  let arrOfString = final.split(','); //делаем числа вместо строки = [1, 2, 3] вместо ["1, 2, 3"]
+  let arrOfNumbers = arrOfString.map(element => {
+    return Number(element);
+  });
+
+  return arrOfNumbers;
 }
